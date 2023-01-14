babb9beb21c3df4e417138d56acfe88abba4491d
seomar <seomar2022@gmail.com>
2023-01-14 14:26:09 +0900

    prepareArray

--- src/encode.java
+++ src/encode.java
@@ -2,5 +2,35 @@ public class encode {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+        char [] plainAlphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+                'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        char [] cipherAlphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+                'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        System.out.println(plainAlphabet);
+
+        int n = 2;
+
+        for(int i = 0; i<plainAlphabet.length; i++){
+            cipherAlphabet[i] = plainAlphabet[(i+n) % plainAlphabet.length];
+        }
+        String plainText = "happy";
+        char [] plainTextToChar = plainText.toCharArray();
+
+        for(char letter: plainTextToChar){
+            System.out.println(plainAlphabet.indexOf(letter));
+
+        }
+        System.out.println(plainTextToChar);
+        System.out.println(plainTextToChar[0]);
+
+
+        System.out.println(plainAlphabet);
+        System.out.println(cipherAlphabet);
+
     }
+//
+//    public static boolean encode(String str, int n){
+//
+//        return true;
+//    }
 }
