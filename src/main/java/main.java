89281bdfdb6ceaa91da28a5e612fa4ca1db8c99d
seomar <seomar2022@gmail.com>
2023-01-26 13:35:35 +0900

    D+10 practiceCrawler

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,6 +1,32 @@
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
 public class main {
 
     public static void main(String[] args) {
+
+        try {
+            //対象となるurl
+            String URL = "https://www.dictionary.com/browse/apple";
+
+            // Connection
+            Connection conn = Jsoup.connect(URL);
+
+            // HTML parsing
+            Document html = conn.get(); // conn.post();
+
+            // 4. HTML 出力
+            System.out.println( html.toString() );
+            // System.out.println(html.getElementsByClass("h1"));
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         caesarCipher caesarCipher = new caesarCipher();
 
 //        System.out.println(caesarCipher.encode("apple", 2));
