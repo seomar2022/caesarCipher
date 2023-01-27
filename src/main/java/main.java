f1526b033ffc12d7384514e073ed4623f9ec9031
seomar <seomar2022@gmail.com>
2023-01-27 13:08:46 +0900

    D+10 practiceCrawler2

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,31 +1,50 @@
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
 
 public class main {
 
     public static void main(String[] args) {
+        String URL = "https://en.dict.naver.com/#/search?query=askldjf;ae";
+        Document doc;
 
+        //https://jul-liet.tistory.com/209
         try {
-            //対象となるurl
-            String URL = "https://www.dictionary.com/browse/apple";
-
-            // Connection
-            Connection conn = Jsoup.connect(URL);
-
-            // HTML parsing
-            Document html = conn.get(); // conn.post();
-
-            // 4. HTML 出力
-            System.out.println( html.toString() );
-            // System.out.println(html.getElementsByClass("h1"));
-
+            doc = Jsoup.connect(URL).get();
+            System.out.println( doc.toString() );
+            Elements elem = doc.select(".section section_empty");
+            System.out.println(elem);
 
         } catch (IOException e) {
+            // TODO Auto-generated catch block
             e.printStackTrace();
         }
+
+        ///////////////////////////////////
+
+//        try {
+//            //対象となるurl
+//            String URL = "https://en.wiktionary.org/wiki/askdfj;asd";
+//
+//            // Connection
+//            Connection conn = Jsoup.connect(URL);
+//
+//            // HTML parsing
+//            Document html = conn.get(); // conn.post();指定したページの内容を持ってくる
+//
+//            // 4. HTML 出力
+//        //    System.out.println( html.toString() );
+//           // Elements elem = html.select(".plainlinks");
+//          //  System.out.println(elem);
+//            System.out.println(html.getElementsByClass(".plainlinks"));
+//
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
         caesarCipher caesarCipher = new caesarCipher();
 
