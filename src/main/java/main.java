9e5132e5282ecc79a0f4ecce5b4b93092d508d20
seomar <seomar2022@gmail.com>
2023-02-02 13:37:01 +0900

    D+16

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,3 +1,8 @@
+import org.jsoup.HttpStatusException;
+import org.jsoup.Jsoup;
+
+import java.io.IOException;
+
 public class main {
 
     public static void main(String[] args) {
@@ -8,15 +13,37 @@ public class main {
 //        System.out.println(caesarCipher.encode("hello world", 0));
 //        System.out.println(caesarCipher.encode("banana"));
 //        System.out.println(caesarCipher.encode("kiwi"));
-//        System.out.println(caesarCipher.encode("hello kojima"));
+//        System.out.println(caesarCipher.encode("hello happy world"));
 //        caesarCipher.decode("crrng"); //apple
 //        caesarCipher.decode("igug" ); //kiwi
-        caesarCipher.decode("czggj rjmgy"); //hello world
+       // caesarCipher.decode("czggj rjmgy"); //hello world
+//          caesarCipher.decode("rjmgy czggj"); //world hello
+     //     caesarCipher.decode("lipps lettc asvph"); //hello happy world
 //        caesarCipher.decode("rovvy uytswk"); //hello kojima
 
 //        caesarCipher.decode("ohwwf"); //happy
 
   //      System.out.println("aa");
+
+        String potentialAnswer = "hello happy world";
+        String[] splitPotentialAnswer = potentialAnswer.split(" ");
+        int score = 0;
+        String URL = "https://www.dictionary.com/browse/";
+        for (int i = 0; i < splitPotentialAnswer.length; i++) {
+            String searchedWord = (splitPotentialAnswer[i]);
+            System.out.println(searchedWord);
+            try {
+                Jsoup.connect(URL).get();
+                score += 1;
+                System.out.println("↑○　辞書にある単語");
+            } catch (HttpStatusException httpStatusException) {
+                //IOExceptionの中にHttpStatusExceptionがあるので、IOExceptionより先にHttpStatusExceptionをcatch
+                System.out.println("↑X　辞書にない単語");
+            } catch (IOException e) {
+                e.printStackTrace();
+
+            }
+        }
     }
 
 
